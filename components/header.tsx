5baf415834692d45a74a3ab5b19aa6f179467487
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-07-29 09:25:29 +0900

    Create header.tsx

---
+++ components/header.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header({ isTop = false }: { isTop?: boolean }) {
+  return (
+    <>
+      <header id={isTop ? "homeHeader" : undefined}>
+        <div class="headerWrap">
+          <h1 class="siteLogo">
+            <Link href="/">
+              <img src="/img/logo.svg" alt="WAZAWAZA" />
+            </Link>
+          </h1>
+
+          <nav class="gnav">
+            <ul class="gnavMenu">
+              <li>
+                <Link href="/service/">Service</Link>
+              </li>
+              <li>
+                <Link href="/product/">Product</Link>
+              </li>
+              <li>
+                <Link href="/info/">Information</Link>
+              </li>
+              <li>
+                <Link href="/news/">News</Link>
+              </li>
+              <li>
+                <Link href="/blog/">Blog</Link>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <Link href="/line/">Line</Link>
+              </li>
+            </ul>
+            <a
+              href="https://www.instagram.com/wazawaza.inc"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="snsLink"
+            >
+              <img src="/img/instaIcon.svg" alt="" />
+              <span>Instagram</span>
+            </a>
+          </nav>
+
+          <p class="hamburgerMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </p>
+        </div>
+      </header>
+
+      <div class="spFloat">
+        <ul>
+          <li>
+            <Link href="/product/">事例を見る</Link>
+          </li>
+          <li>
+            <a href="#contact">お問い合わせ</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
