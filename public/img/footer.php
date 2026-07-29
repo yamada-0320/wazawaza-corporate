1ced1e3ef0eba72348372eb44b8e8b5646ccc70f
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-07-29 09:47:39 +0900

    Add files via upload

---
+++ public/img/footer.php
@@ -0,0 +1,42 @@
+<footer>
+		<div class="footerInner">
+			<h1 class="siteLogo">
+				<a href="/wazawaza/">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.svg" alt=" WAZAWAZA">
+				</a>
+			</h1>
+
+			<ul class="footerLink">
+			<li>
+          <a href="/wazawaza/service/">Service</a>
+        </li>
+        <li>
+          <a href="/wazawaza/product/">Product</a>
+        </li>
+        <li>
+          <a href="/wazawaza/info/">Information</a>
+        </li>
+        <li>
+          <a href="/wazawaza/blog/">Blog</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+			</ul>
+      <a href="https://www.instagram.com/wazawaza.inc" target="_blank" class="snsLink">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/instaIcon.svg" alt="">
+				<span>Instagram</span>
+			</a>
+		</div>
+     <p class="copyright"><small>Copyright Wazawaza All Rights Reserved.</small></p>
+
+	</footer>
+
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/picturefill.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/slick.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/custom.js"></script>
+  <?php wp_footer(); ?>
+</body>
+</html>
+
