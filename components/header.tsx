9caac7021da858a6d4ea3ada7078a6da1e45484e
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-07-30 08:26:54 +0900

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,67 +1,80 @@
-import Link from "next/link";
-import Image from "next/image";
+"use client";
 
-export default function Header({ isTop = false }: { isTop?: boolean }) {
+import { useState } from "react";
+import Link from "next/link";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <>
-      <header id={isTop ? "homeHeader" : undefined}>
-        <div className="headerWrap">
-          <h1 className="siteLogo">
-            <Link href="/">
-              <img src="/img/logo.svg" alt="WAZAWAZA" />
-            </Link>
-          </h1>
-          <nav className="gnav">
-            <ul className="gnavMenu">
-              <li>
-                <Link href="/service/">Service</Link>
-              </li>
-              <li>
-                <Link href="/product/">Product</Link>
-              </li>
-              <li>
-                <Link href="/info/">Information</Link>
-              </li>
-              <li>
-                <Link href="/news/">News</Link>
-              </li>
-              <li>
-                <Link href="/blog/">Blog</Link>
-              </li>
-              <li>
-                <a href="#contact">Contact</a>
-              </li>
-              <li>
-                <Link href="/line/">Line</Link>
-              </li>
-            </ul>
-            <a
-              href="https://www.instagram.com/wazawaza.inc"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="snsLink"
-            >
-              <img src="/img/instaIcon.svg" alt="" />
-              <span>Instagram</span>
-            </a>
-          </nav>
-          <p className="hamburgerMenu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </p>
+    <header className="header">
+      <div className={`headerWrap ${isOpen ? "bgBlack" : ""}`}>
+        <h1 className="headerLogo">
+          <Link href="/" onClick={closeMenu}>
+            <img src="/img/logo.svg" alt="WAZAWAZA" />
+          </Link>
+        </h1>
+
+        <nav className={`gnav ${isOpen ? "js-slide" : ""}`} style={{ display: isOpen ? "block" : "none" }}>
+          <ul className="gnavNav">
+            <li>
+              <Link href="/service/" onClick={closeMenu}>
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link href="/product/" onClick={closeMenu}>
+                Product
+              </Link>
+            </li>
+            <li>
+              <Link href="/info/" onClick={closeMenu}>
+                Information
+              </Link>
+            </li>
+            <li>
+              <Link href="/news/" onClick={closeMenu}>
+                News
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog/" onClick={closeMenu}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link href="/#contact" onClick={closeMenu}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link href="/line/" onClick={closeMenu}>
+                Line
+              </Link>
+            </li>
+            <li className="insta">
+              <a href="https://www.instagram.com/wazawaza.inc" target="_blank" rel="noopener noreferrer">
+                <img src="/img/iconInsta.svg" alt="Instagram" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* ハンバーガーボタン */}
+        <div className={`hamburgerMenu ${isOpen ? "is-active" : ""}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-      </header>
-      <div className="spFloat">
-        <ul>
-          <li>
-            <Link href="/product/">事例を見る</Link>
-          </li>
-          <li>
-            <a href="#contact">お問い合わせ</a>
-          </li>
-        </ul>
       </div>
-    </>
+    </header>
   );
 }
