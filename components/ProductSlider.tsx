8ff7a2047f0864a9ae9a18d38ea39ffc26381797
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-08-06 10:23:30 +0900

    Update ProductSlider.tsx

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -57,7 +57,7 @@ export default function ProductSlider({ posts }: Props) {
                 style={{
                   display: "block",
                   width: "100%",
-                  aspectRatio: "16 / 10",
+                  aspectRatio: "16 / 9",
                   overflow: "hidden",
                 }}
               >
