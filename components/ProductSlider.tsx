9aadd3459bc465c9f0545c6576643fce7357e1be
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-08-07 13:36:07 +0900

    Update ProductSlider.tsx

--- components/ProductSlider.tsx
+++ components/ProductSlider.tsx
@@ -1,94 +1,11 @@
+import { getRecentProducts } from "@/lib/microcms";
+import ProductSliderClient from "./ProductSliderClient";
 
+export default async function ProductSlider() {
+  // サーバー側で安全に microCMS からデータを取りに行く
+  const response = await getRecentProducts(8);
+  const posts = response?.contents || [];
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import { getRecentProducts, type Product } from "@/lib/microcms";
-
-// Swiper基本スタイルの読み込み
-import "swiper/css";
-
-export default function ProductSlider() {
-  const [posts, setPosts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    // コンポーネント単体で microCMS から最新8件を取得
-    getRecentProducts(8).then((res) => {
-      if (res?.contents) {
-        setPosts(res.contents);
-      }
-    });
-  }, []);
-
-  if (!posts || posts.length === 0) {
-    return (
-      <div className="productSider">
-        <p className="empty_list">事例はありません</p>
-      </div>
-    );
-  }
-
-  // ループ動作を安定させるため、投稿数が6件未満の場合は複製
-  const displayPosts =
-    posts.length < 6 ? [...posts, ...posts, ...posts] : posts;
-
-  return (
-    <div className="productSider" style={{ width: "100%", overflow: "hidden" }}>
-      <Swiper
-        modules={[Autoplay]}
-        spaceBetween={24}
-        slidesPerView={1.3}
-        centeredSlides={true}
-        loop={true}
-        speed={1000}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2.2,
-            centeredSlides: false,
-          },
-          1024: {
-            slidesPerView: 3.2,
-            centeredSlides: false,
-          },
-        }}
-        style={{ width: "100%", padding: "10px 0" }}
-      >
-        {displayPosts.map((item, index) => {
-          const thumb =
-            item.thumbnail?.url || "/assets/img/common/no_image.jpg";
-          return (
-            <SwiperSlide key={`${item.id}-${index}`}>
-              <Link
-                href={`/product/detail/?id=${item.id}`}
-                className="sliderItem"
-                style={{
-                  display: "block",
-                  width: "100%",
-                  aspectRatio: "4 / 3",
-                  overflow: "hidden",
-                }}
-              >
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={thumb}
-                  alt={item.title}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                    display: "block",
-                  }}
-                />
-              </Link>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </div>
-  );
+  // Swiperのアニメーション部分（Client）に取得したデータを渡す
+  return <ProductSliderClient posts={posts} />;
 }
