ad50e3b069fadd6c989b5fad20f1fb73df1cb35d
YOKO Yamada <129817824+yamada-0320@users.noreply.github.com>
2026-08-07 13:13:54 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect } from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import Contact from "@/components/contact";
+import MvTitle from "@/components/MvTitle";
 
 export default function Home() {
   useEffect(() => {
@@ -14,30 +14,6 @@ export default function Home() {
     }
   }, []);
 
-  // 白帯のアニメーション設定
-  const barAnimation = {
-    hidden: { x: "-100%" },
-    visible: {
-      x: "100%",
-      transition: {
-        duration: 1.3,
-        delay: 0.8,
-        ease: [0.77, 0, 0.175, 1],
-      },
-    },
-  };
-
-  const textAnimation = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: {
-        duration: 0.6,
-        delay: 1.3,
-        ease: "easeInOut",
-      },
-    },
-  };
 
   const svgStyle = {
     fill: "none",
@@ -52,76 +28,7 @@ export default function Home() {
       <section className="scMv" style={{ display: "block" }}>
         <div className="mvWrap">
           <div className="passing-box titleArea">
-            <h2 className="mvTitle">
-              <div
-                className="mv-passing-bar"
-                style={{
-                  position: "relative",
-                  display: "inline-block",
-                  overflow: "hidden",
-                }}
-              >
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={barAnimation}
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    background: "#fff",
-                    zIndex: 2,
-                  }}
-                />
-                <motion.span
-                  className="mv-passing-txt"
-                  initial="hidden"
-                  animate="visible"
-                  variants={textAnimation}
-                  style={{ position: "relative", zIndex: 1, display: "block" }}
-                >
-                  ワザワザやる
-                </motion.span>
-              </div>
-
-              <br />
-
-              <div
-                className="mv-passing-bar"
-                style={{
-                  position: "relative",
-                  display: "inline-block",
-                  overflow: "hidden",
-                  marginTop: "1rem",
-                }}
-              >
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={barAnimation}
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    background: "#fff",
-                    zIndex: 2,
-                  }}
-                />
-                <motion.span
-                  className="mv-passing-txt"
-                  initial="hidden"
-                  animate="visible"
-                  variants={textAnimation}
-                  style={{ position: "relative", zIndex: 1, display: "block" }}
-                >
-                  ワクワクする
-                </motion.span>
-              </div>
-            </h2>
+            <MvTitle />
           </div>
 
           <div className="mvLogoArea">
